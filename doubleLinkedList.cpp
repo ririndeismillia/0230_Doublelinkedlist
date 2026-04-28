87e37a89be8aae920936df0bffc005b2d579515e
ririndeismillia <ririndeismillia@gmail.com>
2026-04-28 15:10:04 +0700

    Traverse the list to find the node

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -96,5 +96,15 @@ public:
 
         Node *current = START;
 
+        // Step 1: Traverse the list to find the node
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
+        
     }
 };
