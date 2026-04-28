614ad9077eff8841b5a5d92bda14ea67a17dff2f
ririndeismillia <ririndeismillia@gmail.com>
2026-04-28 15:23:44 +0700

    membuat case

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -229,5 +229,31 @@ int main()
          cout << "6. Exit\n";
          cout << "Enter your choice: ";
 
-    }
+         cin >> choice;
+
+         switch (choice)
+         {
+         case '1':
+              list.addNode();
+              break;
+         case '2':
+               list.hapus();
+               break;
+         case '3':
+               list.traverse();
+               break;
+         case '4':
+               list.revtraverse();
+               break; 
+         case '5':
+               list.searchData();
+               break;
+         case '6':
+               return 0;
+         default:
+               cout << "Invalid option\n";                                  
+         }
+     }
 };
+        
+
