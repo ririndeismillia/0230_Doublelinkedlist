42d8d29a5019335737b41b3373a8af379e811e39
ririndeismillia <ririndeismillia@gmail.com>
2026-04-28 15:03:23 +0700

    Allocate value memory for new node

---
+++ doubleLinkedList.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
+};
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+    
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        // Step 1: Allocate value memory for new node
+        Node *newNode = new Node();
+    }
+};
+        
